ae58062a5451535b89755edb4504e2c2aba7bce7
rainpends <rppendon@up.edu.ph>
2023-05-24 13:48:11 +0800

    Created signUp function.

--- server/auth-controller.js
+++ server/auth-controller.js
@@ -4,8 +4,36 @@ const jwt = require("jsonwebtoken")
 //  get user model registered in Mongoose
 const User = mongoose.model("user")
 
-exports.signUp = (req, res) => {
+exports.signUp = async (req, res) => {
+    try{
+        const {fname, mname, lname, suffix, studentno, userType, email, password, applications, adviser} = req.body
 
+        const existingUser = await User.findOne({email})
+
+        if(existingUser) {
+            return res.send({success: false})
+        }
+
+        const newuser = new User({
+            fname,
+            mname,
+            lname,
+            suffix,
+            studentno,
+            userType,
+            email,
+            password,
+            applications,
+            adviser
+        })
+
+        await newuser.save()
+
+        res.send({success: true})
+    }
+    catch (err) {
+        return res.send({success: false})
+    }
 }
 
 exports.login = (req, res) => {
@@ -13,5 +41,5 @@ exports.login = (req, res) => {
 }
 
 exports.checkIfLoggedIn = (req, res) => {
-    
+
 }
